a69ac779e5ea85b453b6ca383830d17fd7494801
Sharon Gray <smgray113@gmail.com>
2017-07-12 23:18:02 -0700

    Create a html webpage about ordering pizza.

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+function Order () {
+  this.size = "";
+  this.toppings = [];
+  this.cost = 0;
+}
+
+Order.prototype.calculateCost = function() {
+  if (this.size === "8-inch Small Thin Crust- $9.00" || this.size === "8-inch Small Original Crust- $9.00") {
+    this.cost += 9;
+  }
+  else if (this.size === "12-inch Medium Thin Crust- $12.00" || this.size === "12-inch Medium Original Crust- $12.00") {
+    this.cost += 12;
+  }
+  else if (this.size === "16-inch Large Thin Crust- $15.00" || this.size === "16-inch Large Original Crust- $15.00") {
+    this.cost += 15;
+  }
+}
+
+$(document).ready(function() {
+  var order = new Order();
+  $("#orderSubmit").click(function() {
+    console.log("hello");
+    var inputtedSize = $("#size").val();
+    order.size = inputtedSize;
+    console.log(inputtedSize);
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      var inputtedToppings = $(this).val();
+      order.toppings.push(inputtedToppings);
+    });
+    order.cost += order.toppings.length;
+    order.calculateCost();
+    console.log(order.cost);
+    console.log(order);
+  });
+});
