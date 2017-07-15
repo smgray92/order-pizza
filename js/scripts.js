00721aeca7bfb7442ea2f6c02fbdc6cad7d928bc
Sharon Gray <smgray113@gmail.com>
2017-07-15 13:10:00 -0700

    Remove the toppings list display in the review order section, and check for indentations.

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,7 @@ function Order () {
   this.cost = 0;
 }
 
-
-
 Order.prototype.calculateCost = function() {
-  // this.cost += this.toppings.length;
-  // this.size.forEach(function() {
-  // for (var size in Order) {
-  //   if(Order.hasOwnProperty(size)){
-  //     console.log("yes");
-  //   }
-// array.forEach(function() {
   if (this.size === "8-inch Small Thin Crust- $9.00" || this.size === "8-inch Small Original Crust- $9.00") {
     this.cost += 9;
   }
@@ -24,11 +15,8 @@ Order.prototype.calculateCost = function() {
   }
   else if (this.size === "16-inch Large Thin Crust- $15.00" || this.size === "16-inch Large Original Crust- $15.00") {
     this.cost += 15;
-
+  }
 }
-}
-
-
 
 function Contact (name, street, [], phone) {
   this.name = name;
@@ -52,41 +40,38 @@ Address.prototype.delivery = function() {
   return this.city + ", " + this.state + " " + this.zip;
 }
 
-
 $(document).ready(function() {
   $("#anotherPizza").click(function() {
-    // $("input:checkbox[name=toppings]").prop("checked", false);
     $("#newPizza").append("<div class='col-md-3'>" + "<div class='newPizza'>" +
-"<h4>Size:</h4>" +
-"<select class='form-control' id='size'>" +
-  "<option>8-inch Small Thin Crust- $9.00</option>" +
-  "<option>8-inch Small Original Crust- $9.00</option>" +
-  "<option>12-inch Medium Thin Crust- $12.00</option>" +
-  "<option>12-inch Medium Original Crust- $12.00</option>" +
-  "<option>16-inch Large Thin Crust- $15.00</option>" +
-  "<option>16-inch Large Original Crust- $15.00</option>" +
-"</select>" +
-"<h4>Toppings ($1.00 each):</h4>" +
-"<input type='checkbox' name='toppings' value='pepperoni'>Pepperoni<br>" +
-"<input type='checkbox' name='toppings' value='mozzarella'>Mozzarella<br>" +
-"<input type='checkbox' name='toppings' value='sausage'>Italian Sausage<br>" +
-"<input type='checkbox' name='toppings' value='bacon'>Bacon<br>" +
-"<input type='checkbox' name='toppings' value='chicken'>Grilled Chicken<br>" +
-"<input type='checkbox' name='toppings' value='onion'>Onion<br>" +
-"<input type='checkbox' name='toppings' value='pepper'>Green Pepper<br>" +
-"<input type='checkbox' name='toppings' value='garlic'>Garlic<br>" +
-"<input type='checkbox' name='toppings' value='tomato'>Tomato<br>" +
-"<input type='checkbox' name='toppings' value='mushroom'>Mushroom<br>" +
-"<input type='checkbox' name='toppings' value='olive'>Olives<br>" +
-"</div>" + "</div>");
+    "<h4>Size:</h4>" +
+    "<select class='form-control' id='size'>" +
+    "<option>8-inch Small Thin Crust- $9.00</option>" +
+    "<option>8-inch Small Original Crust- $9.00</option>" +
+    "<option>12-inch Medium Thin Crust- $12.00</option>" +
+    "<option>12-inch Medium Original Crust- $12.00</option>" +
+    "<option>16-inch Large Thin Crust- $15.00</option>" +
+    "<option>16-inch Large Original Crust- $15.00</option>" +
+    "</select>" +
+    "<h4>Toppings ($1.00 each):</h4>" +
+    "<input type='checkbox' name='toppings' value='pepperoni'>Pepperoni<br>" +
+    "<input type='checkbox' name='toppings' value='mozzarella'>Mozzarella<br>" +
+    "<input type='checkbox' name='toppings' value='sausage'>Italian Sausage<br>" +
+    "<input type='checkbox' name='toppings' value='bacon'>Bacon<br>" +
+    "<input type='checkbox' name='toppings' value='chicken'>Grilled Chicken<br>" +
+    "<input type='checkbox' name='toppings' value='onion'>Onion<br>" +
+    "<input type='checkbox' name='toppings' value='pepper'>Green Pepper<br>" +
+    "<input type='checkbox' name='toppings' value='garlic'>Garlic<br>" +
+    "<input type='checkbox' name='toppings' value='tomato'>Tomato<br>" +
+    "<input type='checkbox' name='toppings' value='mushroom'>Mushroom<br>" +
+    "<input type='checkbox' name='toppings' value='olive'>Olives<br>" +
+    "</div>" + "</div>");
   });
 
   $("#orderSubmit").click(function() {
-var order = new Order();
+    var order = new Order();
     $("#reviewOrder").show();
     $(".img-hide").show();
 
-    // $("input:checkbox[name=toppings]").prop("checked", false);
     var address = $("input:radio[name=service]:checked").val();
     if (address === "delivery") {
       $("#addressDelivery").show();
@@ -114,66 +99,38 @@ var order = new Order();
     $('.show-address').text(addressName.delivery());
     $('.show-phone').text(contact.phone);
 
+    $(".newPizza").each(function() {
+      var inputtedSize = $(this).find("#size").val();
+      order.size = inputtedSize;
+      order.calculateCost();
+      array.push(inputtedSize);
 
-$(".newPizza").each(function() {
+      console.log(inputtedSize);
+      console.log(order.cost);
+    });
+    console.log(array);
+    array.forEach(function(item) {
+      $("ul#pizza").append("<li><span class='pizzaList'>" + item + "</span></li>");
+    });
+    // $(".pizzaList").one( 'click', function() {
+    //   $(".titleToppings").append("<p>Toppings:</p>" + "<ul id='toppingsList'>" + "</ul>");
+    //   $("ul#toppingsList").each(function() {
+    //
+    //     order.toppings.forEach(function(item) {
+    //       $("ul#toppingsList").append("<li>" + item + " - $1.00</li>");
+    //     });
 
-    var inputtedSize = $(this).find("#size").val();
-        order.size = inputtedSize;
-        order.calculateCost();
-        array.push(inputtedSize);
-
-console.log(inputtedSize);
-console.log(order.cost);
-  });
-console.log(array);
-array.forEach(function(item) {
-    $("ul#pizza").append("<li><span class='pizzaList'>" + item + "</span></li>");
-  });
-    // $(".pizzaList").each(function() {
-    // $(this).click(function() {
-      // $(".toppings").empty();
-      $(".pizzaList").one( 'click', function() {
-        $(".titleToppings").append("<p>Toppings:</p>");
-        // $(".pizzaList").each(function() {
-      // $("#detailedOrder").show();
-      order.toppings.forEach(function(item) {
-        $("ul.titleToppings").append("<li>" + item + " - $1.00</li>");
-
-        // order.calculateCost();
-      });
-      });
-
-  // });
-     $("input:checkbox[name=toppings]:checked").each(function(){
-    var inputtedToppings =  $(this).val();
-    // var inputtedToppings = $("input:checkbox[name=toppings]:checked").map(function(){
-    // return this.value;
-  // }).get().join(",");
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      var inputtedToppings =  $(this).val();
       order.toppings.push(inputtedToppings);
-
       console.log(inputtedToppings);
     });
 
-
     order.cost += order.toppings.length;
     $(".totalCost").empty();
-
     $(".totalCost").append("$" + order.cost + ".00");
     console.log(order.cost);
-    // $("ul#pizza").append("<li><span class='pizzaList'>" + order.size + "</span></li>");
-    // // $(".pizzaList").each(function() {
-    // // $(this).click(function() {
-    //   // $(".toppings").empty();
-    //   $(".pizzaList").click(function() {
-    //   $("#detailedOrder").show();
-    //   order.toppings.forEach(function(item) {
-    //     $("ul#toppingsReview").append("<li>" + item + " - $1.00</li>");
-    //   });
-    //   });
 
     console.log(order);
+  });
 });
-
-
-});
-// });
